02d56065177bb0d201b7eb713f5126ee27022fe2
TK ahn <119566699+TGahn747@users.noreply.github.com>
2023-02-04 16:28:01 +0900

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def text():
     #Mark Down
-    st.markdown('Markdown')
+#     st.markdown('Markdown')
     st.markdown('Streamlit is **_really_ cool**.')
     st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
     st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
