441838c3aa9f3a4723d5ee9d1b57fe4a40fddb56
TK ahn <119566699+TGahn747@users.noreply.github.com>
2023-02-07 10:49:00 +0900

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -86,20 +86,3 @@ if button:
     st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")    
     
     
-<div class="group_nav">
-<ul class="list_nav type_fix">
-<li class="nav_item">
-<a href="https://mail.naver.com/" class="nav" data-clk="svc.mail"><i class="ico_mail"></i>메일</a>
-</li>
-<li class="nav_item"><a href="https://section.cafe.naver.com/" class="nav" data-clk="svc.cafe">카페</a></li>
-<li class="nav_item"><a href="https://section.blog.naver.com/" class="nav" data-clk="svc.blog">블로그</a></li>
-<li class="nav_item"><a href="https://kin.naver.com/" class="nav" data-clk="svc.kin">지식iN</a></li>
-<li class="nav_item"><a href="https://shopping.naver.com/" class="nav shop" data-clk="svc.shopping"><span class="blind">쇼핑</span></a></li>
-<li class="nav_item"><a href="https://shoppinglive.naver.com/home" class="nav shoplive" data-clk="svc.shoppinglive"><span class="blind">쇼핑
-<li class="nav_item"><a href="https://order.pay.naver.com/home" class="nav" data-clk="svc.pay">Pay</a></li>
-<li class="nav_item">
-<a href="https://tv.naver.com/" class="nav" data-clk="svc.tvcast"><i class="ico_tv"></i>TV</a>
-</li>
-</ul>
-<ul class="list_nav NM_FAVORITE_LIST">
-<li class="nav_item"><a href="https://dict.naver.com/" class="nav" data-clk="svc.dic">사전</a></li>    
